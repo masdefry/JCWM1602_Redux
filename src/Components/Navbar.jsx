b7e3a6ea5afdec8276aad6f426550ec5514c4a07
masdefry <muhammaddefryan@gmail.com>
2021-03-05 09:46:10 +0700

    Update Carts With Window Location

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import React from 'react'
+
+export default class Navbar extends React.Component{
+
+    state = {
+        totalCarts: 0
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:2000/carts?userId=3')
+        .then((res) => {
+            console.log(res.data.length)
+            this.setState({totalCarts: res.data.length})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    render(){
+        return(
+            <div className='bg-warning' style={{height: '75px'}}>
+                <div className="container">
+                    <div className="row align-items-center" style={{height: '75px'}}>
+                        <div className="col-12">
+                            Keranjang Belanja Anda : {
+                                this.state.totalCarts?
+                                    this.state.totalCarts
+                                :
+                                    0
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/DetailProduct.jsx
@@ -0,0 +1,81 @@
+import axios from "axios"
+import React from "react"
+
+class DetailProduct extends React.Component {
+
+    updateCart = () => {
+        let dataToSend = {
+            userId: 3,
+            productId: 2,
+            quantity: 1
+        }
+
+        axios.post('http://localhost:2000/carts', dataToSend)
+        .then((res) => {
+            console.log(res)
+            window.location = '/detail-product'
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+    render () {
+        return (
+            <div className= "container">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div>
+                            <img src="https://d2xjmi1k71iy2m.cloudfront.net/dairyfarm/id/images/728/0172811_PE327002_S4.jpg" className="img-fluid" alt=""/>
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="mt-5">
+                            <h1>
+                                Meja Kantor
+                            </h1>
+                            <p className="font-italic">
+                                Sold : 2 Products
+                            </p>
+                            <h3>
+                                Rp. 5.000.000
+                            </h3>
+                            <hr/>
+                        </div>
+
+                        <div>
+                            <p>
+                                Stock : 20 items
+                            </p>
+
+                            <p>
+                                Weight: 7 kg
+                            </p>
+                            <hr/>
+                        </div>
+
+                        <div>
+                            <h5>
+                                Category :
+                            </h5>
+                            <p>
+                                Perabotan Kantor
+                            </p>
+                        </div>
+
+                        <div className="mt-5 mb-3 d-flex justify-content-center">
+                            <input type="button" value="Add To Cart" className="btn btn-primary" style={{width: "300px"}} onClick={this.updateCart} />
+                        </div>
+
+
+                    </div>
+    
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default DetailProduct
